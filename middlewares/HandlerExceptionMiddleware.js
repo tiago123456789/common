34b1a9438fd691ca27db0bd3fd2d5762c1a34eb9
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2020-09-20 13:07:01 -0300

    Create handler exception

---
+++ middlewares/HandlerExceptionMiddleware.js
@@ -0,0 +1,25 @@
+module.exports = (error, request, response, next) => {
+
+    switch(error.name) {
+        case "BusinessLogicException": 
+            return response.status(409).json({
+                statusCode: 409,
+                message: error.message
+            });
+        case "NotFoundException":
+            return response.status(404).json({
+                statusCode: 404,
+                message: error.message
+            });
+        case "InvalidDatasException":
+            return response.status(400).json({
+                statusCode: 400,
+                message: JSON.parse(error.message)
+            });
+        default: 
+            return response.status(500).json({
+                statusCode: 500,
+                message: "Internal server error."
+            });
+    }
+}
